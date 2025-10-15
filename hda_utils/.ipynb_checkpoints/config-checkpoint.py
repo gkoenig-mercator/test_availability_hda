11f82cc6c1d2c82478171429310bdf9418e19a2f
gkoenig <gkoenig@mercator-ocean.fr>
2025-10-15 07:23:56 +0000

    Remove files that should not have been there

--- hda_utils/.ipynb_checkpoints/config-checkpoint.py
+++
@@ -1,6 +0,0 @@
-# hda_utils/config.py
-from hda import Client, Configuration
-
-def get_client():
-    config = Configuration(path='../.hdarc')
-    return Client(config=config, retry_max=3, sleep_max=1)
